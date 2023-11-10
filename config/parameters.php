c68923ae641926773cf9902d0f0b2ea7c01f3cdc
DylanBernardi <147556161+DylanBernardi@users.noreply.github.com.>
2023-11-10 11:37:23 -0300

    htaccess added and changed the url

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
 
-define("base_url", "http://localhost/masterphp/proyecto-poo/");
+define("base_url", "http://localhost/masterphp/proyecto-php-poo/");
 define("default_controller", "productoController");
 define("default_action", "index");
